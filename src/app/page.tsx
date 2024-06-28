8cc5a48d30ea9775fd95b75e49fdca70652436f5
abhay gupta <akg568794@gmail.com>
2024-06-29 02:39:22 +0530

    Added Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from '@/components/Banner'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    <h1>Welcome to Coder's Book</h1>
+    <Banner/>
     </>
   )
 }
